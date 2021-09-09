fa2b3638e1062e6c3096e81c8cbd6fb6e175a342
Xingchen Hong <1815174+Zodiase@users.noreply.github.com>
2021-09-08 22:05:19 -0700

    Update tsconfig. (#6)
    * Update tsconfig.
    
    - Enable strict mode.
    - Update build dir to "build" instead of "lib" to avoid confusion.
    - Other minor changes to stay consistent with my other TS projects.
    - add npm command "build".
    
    * Fix type error in tests.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,7 +2,7 @@ import lib from './index';
 
 describe('lib', () => {
     it('does something', async () => {
-        const input = [];
+        const input = [1];
         const output = await lib(input);
         expect(output).toBe(input);
     });
